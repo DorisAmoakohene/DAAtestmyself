86950e440541efa52481ddf6ec9c4252a8996d6b
Doris Amoakohene <143125519+DorisAmoakohene@users.noreply.github.com>
2023-09-04 11:28:31 -0700

    Update myselftestr.R
    i added a new line to line 16

--- myselftestr.R
+++ myselftestr.R
@@ -13,6 +13,8 @@ X.mat <- as.matrix(SAheart.dt[,-..label.col.i ])
 X.mat
 str(X.mat)
 
+###Test myself in github
+
 if(!file.exists("zip.test.gz")){download.file("https://web.stanford.edu/~hastie/ElemStatLearn/datasets/zip.test.gz","zip.test.gz")}
 zip.dt<- data.table::fread("zip.test.gz")
 zip.mat <-as.matrix(zip.dt[,-1])
